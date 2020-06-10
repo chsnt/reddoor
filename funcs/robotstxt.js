352bae04b8302eed3e2557234e73901da8758b5d
chsnt <43132492+chsnt@users.noreply.github.com>
2020-06-10 17:34:24 +0300

    arrows fix 90px

--- funcs/robotstxt.js
+++ funcs/robotstxt.js
@@ -4,8 +4,8 @@ module.exports = () => {
     let links = []
     // /catalog/Health
     catalog.forEach(group => {
-        links.push(`Disallow: /catalog/${group.name.en}`)
-        group.list.forEach(board => links.push(`Disallow: /r/${board.en}`))
+        links.push(`Disallow: /catalog/${group.name.en}$`)
+        group.list.forEach(board => links.push(`Disallow: /r/${board.en}$`))
     })
     console.log(links)
     return links.join('\n')
